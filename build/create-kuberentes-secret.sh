2fde301d6d9b85b2788e901d37cac97e9bb479bc
Oleg Sucharevich <olegs@codefresh.io>
2020-01-31 09:36:16 +0200

    fix secret generation script

--- build/create-kuberentes-secret.sh
+++ build/create-kuberentes-secret.sh
@@ -1,8 +1,8 @@
 echo "Creating secret in kuberentes cluster"
 
-kubectl create secret generic jira-sync \ 
-    --from-literal=slack-webhook-url=$SLACK_WEBHOOK_URL \ 
-    --from-literal=jira-user=$JIRA_USER \ 
-    --from-literal=jira-endpoint=$JIRA_ENDPOINT \ 
+kubectl create secret generic jira-sync \
+    --from-literal=slack-webhook-url=$SLACK_WEBHOOK_URL \
+    --from-literal=jira-user=$JIRA_USER \
+    --from-literal=jira-endpoint=$JIRA_ENDPOINT \
     --from-literal=jira-api-token=$JIRA_API_TOKEN
 
